502f02df04f50aa1eb642e3a4ab8f63b38934512
Bart Massey <bart@cs.pdx.edu>
2024-04-01 15:48:03 -0700

    added more to example

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+struct Point {
+    x: i64,
+    y: i64,
+}
+
 fn main() {
-    for i in (1 ..= 10).rev() {
-        println!("{}", i);
+    let p = Point { x: 0, y: 0 };
+    println!("{} {}", p.x, p.y);
+
+    for i in (1u8 ..= 10).rev() {
+        let x: u16 = i.into();
+        println!("{}", x);
     }
 }
