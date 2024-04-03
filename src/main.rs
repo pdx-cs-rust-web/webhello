8993695984912c1750fbc54b95d5549d5e66ac17
Bart Massey <bart.massey@gmail.com>
2024-04-02 23:18:01 -0700

    cleaned up IP address def, used 127.0.0.1 for security

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 // https://github.com/programatik29/axum-tutorial/blob/master/tutorial/02-layout.md
 // https://docs.rs/axum/latest/axum/
 
+use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+
 use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, world!" }));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let ip = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000);
+    let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
