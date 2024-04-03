c821ae7fe56bba55d56186520e28e23c961cfc3d
Bart Massey <bart.massey@gmail.com>
2024-04-03 15:23:00 -0700

    got server working

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,13 @@
 use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
-
-/*
-pub enum Option<T> {
-    None,
-    Some(T),
-}
-pub use Option::*;
-
-pub enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
-pub use Result::*;
-*/
+use std::io::Write;
 
 fn main() {
     let localhost = Ipv4Addr::new(127, 0, 0, 1);
     let socket_addr = SocketAddrV4::new(localhost, 3000);
     let tcp_listener = TcpListener::bind(socket_addr).unwrap();
     loop {
-        let (_tcp_stream, _) = tcp_listener.accept().unwrap();
-        todo!()
+        let (mut tcp_stream, addr) = tcp_listener.accept().unwrap();
+        println!("connection from {}", addr);
+        write!(tcp_stream, "hello world\r\n").unwrap();
     }
 }
