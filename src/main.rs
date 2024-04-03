4264f729733e6895b9b1bda2db37f9936e668d29
Bart Massey <bart.massey@gmail.com>
2024-04-03 15:05:26 -0700

    started to transmute into raw webserver

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-struct Point {
-    x: i64,
-    y: i64,
+use std::net::{Ipv4Addr, SocketAddrV4, TcpListener};
+
+/*
+pub enum Option<T> {
+    None,
+    Some(T),
 }
+pub use Option::*;
+
+pub enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+pub use Result::*;
+*/
 
 fn main() {
-    let p = Point { x: 0, y: 0 };
-    println!("{} {}", p.x, p.y);
-
-    for i in (1u8 ..= 10).rev() {
-        let x: u16 = i.into();
-        println!("{}", x);
+    let localhost = Ipv4Addr::new(127, 0, 0, 1);
+    let socket_addr = SocketAddrV4::new(localhost, 3000);
+    let tcp_listener = TcpListener::bind(socket_addr).unwrap();
+    loop {
+        let (_tcp_stream, _) = tcp_listener.accept().unwrap();
+        todo!()
     }
 }
