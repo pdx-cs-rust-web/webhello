c921dabc6b2b0c2734ced2e39e361a237b8beee5
Bart Massey <bart.massey@gmail.com>
2024-04-03 00:07:27 -0700

    implemented header parsing; screws up

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
-use std::io::Read;
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream, SocketAddrV4, Ipv4Addr};
 
-fn hello(mut stream: TcpStream) {
-    let mut req = String::new();
-    stream.read_to_string(&mut req).unwrap();
-    for line in req.lines() {
-        eprintln!("{}", line);
+struct Request {
+    request: String,
+    headers: Vec<(String, String)>,
+}
+
+impl Request {
+
+    fn new(stream: &mut TcpStream) -> Self {
+        let mut reader = BufReader::new(stream);
+        let mut buf = String::new();
+        reader.read_line(&mut buf).unwrap();
+        let request = buf.trim_end().to_string();
+        let mut headers = Vec::new();
+        loop {
+            reader.read_line(&mut buf).unwrap();
+            let header = buf.trim_end().to_string();
+            if header.is_empty() {
+                return Self { request, headers };
+            }
+            let fields: Vec<&str> = header.split(": ").collect();
+            if fields.len() != 2 {
+                eprintln!("malformed header: {}", header);
+                continue;
+            }
+            headers.push((fields[0].to_string(), fields[1].to_string()));
+        }
     }
+}
+
+fn hello(mut stream: TcpStream) {
+    let request = Request::new(&mut stream);
+    eprintln!("{}", request.request);
+    for (name, value) in request.headers {
+        eprintln!("{}: {}", name, value);
+    }
+    writeln!(stream, "hello world\r\n").unwrap();
 }
 
 fn main() {
