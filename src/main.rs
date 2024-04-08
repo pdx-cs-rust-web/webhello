b59b68f37246bef95f9cb0b7d9f9132dfa2a31b1
Bart Massey <bart@cs.pdx.edu>
2024-04-08 14:26:23 -0700

    added server start line

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn discard_request(tcp_stream: &mut TcpStream) {
 fn main() {
     let localhost = Ipv4Addr::new(127, 0, 0, 1);
     let socket_addr = SocketAddrV4::new(localhost, 3000);
+    eprintln!("server starts: {}", socket_addr);
     let tcp_listener = TcpListener::bind(socket_addr).unwrap();
     loop {
         let (mut tcp_stream, addr) = tcp_listener.accept().unwrap();
