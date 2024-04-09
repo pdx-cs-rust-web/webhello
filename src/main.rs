671ed77a06dcea0c371cee7afca7e74e5b3d59cb
Bart Massey <bart.massey@gmail.com>
2024-04-08 17:47:39 -0700

    added full request parsing

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,38 @@
 use std::net::{Ipv4Addr, SocketAddrV4, TcpListener, TcpStream};
 use std::io::{BufRead, Write, BufReader};
 
-fn discard_request(tcp_stream: &mut TcpStream) {
-    let mut reader = BufReader::new(tcp_stream);
-    let mut line = String::new();
-    loop {
-        reader.read_line(&mut line).unwrap();
-        let trimmed = line.trim_end();
-        if trimmed.is_empty() {
-            return;
-        }
-        eprintln!("{}", trimmed);
-        line.clear();
-    }
+#[derive(Debug, Clone)]
+struct Request {
+    method: String,
+    path: String,
+    headers: Vec<(String, String)>,
 }
 
-fn main() {
-    let localhost = Ipv4Addr::new(127, 0, 0, 1);
-    let socket_addr = SocketAddrV4::new(localhost, 3000);
-    eprintln!("server starts: {}", socket_addr);
-    let tcp_listener = TcpListener::bind(socket_addr).unwrap();
+fn get_request(tcp_stream: &mut TcpStream) -> Request {
+    let mut reader = BufReader::new(tcp_stream);
+    let mut line = String::new();
+
+    reader.read_line(&mut line).unwrap();
+    let fields: Vec<&str> = line.trim_end().split_whitespace().collect();
+    let method = fields[0].into();
+    let path = fields[1].into();
+
+    let mut headers = Vec::new();
     loop {
-        let (mut tcp_stream, addr) = tcp_listener.accept().unwrap();
-        eprintln!("connection from {}", addr);
-        discard_request(&mut tcp_stream);
+        line.clear();
+        reader.read_line(&mut line).unwrap();
+        let line = line.trim_end();
+        if line.is_empty() {
+            return Request { method, path, headers };
+        }
+        // eprintln!("header: {:?}", line.trim_end());
+        let (name, value) = line.split_once(": ").unwrap();
+        headers.push((name.into(), value.into()));
+    }
+
+}
+
+fn send_page(tcp_stream: &mut TcpStream) {
         write!(tcp_stream, "HTTP/1.0 200 OK\r\n").unwrap();
         write!(tcp_stream, "Content-Type: text/html; charset=utf-8\r\n").unwrap();
         write!(tcp_stream, "\r\n").unwrap();
@@ -36,6 +45,18 @@ fn main() {
         write!(tcp_stream, "<em>hello world🦀</em>").unwrap();
         write!(tcp_stream, "</body>").unwrap();
         write!(tcp_stream, "</html>").unwrap();
+}
+
+fn main() {
+    let localhost = Ipv4Addr::new(127, 0, 0, 1);
+    let socket_addr = SocketAddrV4::new(localhost, 3000);
+    eprintln!("server starts: {}", socket_addr);
+    let tcp_listener = TcpListener::bind(socket_addr).unwrap();
+    loop {
+        let (mut tcp_stream, addr) = tcp_listener.accept().unwrap();
+        let request = get_request(&mut tcp_stream);
+        eprintln!("{}: {} {}", addr, request.method, request.path);
+        send_page(&mut tcp_stream);
         tcp_stream.flush().unwrap();
     }
 }
