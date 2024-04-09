85c7379aefb4ec4a0ce34e7b46b6dedd149f8cb6
Bart Massey <bart.massey@gmail.com>
2024-04-09 01:04:44 -0700

    added routes, favicon support

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::io::{BufRead, Write, BufReader};
 struct Request {
     method: String,
     path: String,
-    headers: Vec<(String, String)>,
+    _headers: Vec<(String, String)>,
 }
 
 fn get_request(tcp_stream: &mut TcpStream) -> Request {
@@ -23,7 +23,7 @@ fn get_request(tcp_stream: &mut TcpStream) -> Request {
         reader.read_line(&mut line).unwrap();
         let line = line.trim_end();
         if line.is_empty() {
-            return Request { method, path, headers };
+            return Request { method, path, _headers: headers };
         }
         // eprintln!("header: {:?}", line.trim_end());
         let (name, value) = line.split_once(": ").unwrap();
@@ -33,18 +33,32 @@ fn get_request(tcp_stream: &mut TcpStream) -> Request {
 }
 
 fn send_page(tcp_stream: &mut TcpStream) {
-        write!(tcp_stream, "HTTP/1.0 200 OK\r\n").unwrap();
-        write!(tcp_stream, "Content-Type: text/html; charset=utf-8\r\n").unwrap();
-        write!(tcp_stream, "\r\n").unwrap();
-        write!(tcp_stream, "<html>").unwrap();
-        write!(tcp_stream, "<head>").unwrap();
-        write!(tcp_stream, "<meta charset=\"UTF-8\"/>").unwrap();
-        write!(tcp_stream, "<title>hello world🦀</title>").unwrap();
-        write!(tcp_stream, "</head>").unwrap();
-        write!(tcp_stream, "<body>").unwrap();
-        write!(tcp_stream, "<em>hello world🦀</em>").unwrap();
-        write!(tcp_stream, "</body>").unwrap();
-        write!(tcp_stream, "</html>").unwrap();
+    write!(tcp_stream, "HTTP/1.0 200 OK\r\n").unwrap();
+    write!(tcp_stream, "Content-Type: text/html; charset=utf-8\r\n").unwrap();
+    write!(tcp_stream, "\r\n").unwrap();
+    write!(tcp_stream, "<html>").unwrap();
+    write!(tcp_stream, "<head>").unwrap();
+    write!(tcp_stream, "<meta charset=\"UTF-8\"/>").unwrap();
+    write!(tcp_stream, "<title>hello world🦀</title>").unwrap();
+    write!(tcp_stream, "</head>").unwrap();
+    write!(tcp_stream, "<body>").unwrap();
+    write!(tcp_stream, "<em>hello world🦀</em>").unwrap();
+    write!(tcp_stream, "</body>").unwrap();
+    write!(tcp_stream, "</html>").unwrap();
+}
+
+fn send_favicon(tcp_stream: &mut TcpStream) {
+    use std::io::Read;
+
+    write!(tcp_stream, "HTTP/1.0 200 OK\r\n").unwrap();
+    write!(tcp_stream, "Content-Type: image/vnd.microsoft.icon\r\n").unwrap();
+    write!(tcp_stream, "\r\n").unwrap();
+    let mut favicon = Vec::new();
+    std::fs::File::open("assets/favicon.ico")
+        .unwrap()
+        .read_to_end(&mut favicon)
+        .unwrap();
+    tcp_stream.write_all(&favicon).unwrap();
 }
 
 fn main() {
@@ -56,7 +70,15 @@ fn main() {
         let (mut tcp_stream, addr) = tcp_listener.accept().unwrap();
         let request = get_request(&mut tcp_stream);
         eprintln!("{}: {} {}", addr, request.method, request.path);
-        send_page(&mut tcp_stream);
+        if request.method != "GET" {
+            write!(tcp_stream, "405 Method Not Allowed\r\n").unwrap();
+        } else {
+            match request.path.as_ref() {
+                "/" => send_page(&mut tcp_stream),
+                "/favicon.ico" => send_favicon(&mut tcp_stream),
+                _ => write!(tcp_stream, "404 Not Found\r\n").unwrap(),
+            }
+        }
         tcp_stream.flush().unwrap();
     }
 }
