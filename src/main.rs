a7c72397dd21508eabfb1e65399606f0747f274f
Bart Massey <bart.massey@gmail.com>
2024-04-10 11:37:41 -0700

    changed to return static index.html

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,20 @@
 // https://docs.rs/axum/latest/axum/
 
 use std::net::{SocketAddr, IpAddr, Ipv4Addr};
+use std::str::FromStr;
 
-use axum::{routing::get, Router};
-
-async fn hello() -> &'static str {
-    "hello world🦀"
-}
+use axum::Router;
+use tower_http::services as tower;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
-
     let ip = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000);
     eprintln!("webhello: serving {}", ip);
     let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
+
+    let mime_type = FromStr::from_str(r#"text/html; encoding="utf-8""#).unwrap();
+    let page = tower::ServeFile::new_with_mime("assets/index.html", &mime_type);
+    let app = Router::new().nest_service("/", page);
+
     axum::serve(listener, app).await.unwrap();
 }
