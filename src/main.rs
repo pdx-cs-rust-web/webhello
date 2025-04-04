ee73760850d987394f4ad0a96e3163c4e3671a58
Bart Massey <bart.massey@gmail.com>
2025-04-03 17:57:14 -0700

    added response saving

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 // Thanks to Google Search Labs AI for DNS lookup code.
 use std::net::{TcpStream, ToSocketAddrs};
+use std::fs::File;
 use std::io::{Write, read_to_string};
 
+fn fetch_page() -> Result<(), Box<dyn std::error::Error>> {
+    let socket_addr = "www.google.com:80".to_socket_addrs()?.next().unwrap();
+    let mut w = TcpStream::connect(socket_addr)?;
+    write!(w, "GET / HTTP/1.0\r\n\r\n")?;
+    let response = read_to_string(&w)?;
+    let mut f = File::create("response.html")?;
+    write!(f, "{}", response)?;
+    Ok(())
+}
+
 fn main() {
-    let socket_addr = "www.google.com:80".to_socket_addrs().unwrap().next().unwrap();
-    let mut w = TcpStream::connect(socket_addr).unwrap();
-    write!(w, "GET / HTTP/1.0\r\n\r\n").unwrap();
-    let response = read_to_string(&w).unwrap();
-    print!("{}", response);
+    fetch_page().unwrap();
 }
